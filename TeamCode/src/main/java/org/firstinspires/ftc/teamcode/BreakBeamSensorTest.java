47159b3bfb515ad5cefd773f913416bf3c8aa447
me25542 <156400125+me25542@users.noreply.github.com>
2026-01-19 17:46:28 -0700

    Spindex tuned

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BreakBeamSensorTest.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+import org.firstinspires.ftc.teamcode.hardware.sensors.BreakBeam;
+
+@TeleOp
+public class BreakBeamSensorTest extends LinearOpMode {
+  private BreakBeam sensor;
+  public void runOpMode() {
+    sensor = new BreakBeam(hardwareMap.get(DigitalChannel.class, "breakBeam"));
+
+    waitForStart();
+
+    while (opModeIsActive()) {
+      telemetry.addData("Beam", sensor.getState());
+      telemetry.update();
+    }
+  }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TuningTurret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TuningTurret.java
@@ -24,88 +24,45 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.hardware.Turret;
+import org.firstinspires.ftc.teamcode.hardware.motors.HomableRotator;
+import org.firstinspires.ftc.teamcode.hardware.sensors.BreakBeam;
 import org.firstinspires.ftc.teamcode.hardware.servos.Axon;
 import org.firstinspires.ftc.teamcode.hardware.servos.DualAxon;
 
 @TeleOp(name = "Tuning turret", group = "Tuning")
 @Config
 public class TuningTurret extends LinearOpMode {
-  public static double speed = 2000;
-  public static double kP = 0;
-  public static double kI = 0;
-  public static double kD = 0;
-  public static double kF = 0;
+  public static double angle = 0;
+  public static double kD = 0.001;
+  public static double kI = 0.05;
+  public static double kP = 0.25;
+  //public static double kF = 0;
 
   @Override
   public void runOpMode() {
     FtcDashboard dashboard = FtcDashboard.getInstance();
     Telemetry dashboardTelemetry = dashboard.getTelemetry();
 
-    DcMotorEx flywheel = hardwareMap.get(DcMotorEx.class, "flywheel");
-    Motor turretRotator = new Motor(hardwareMap, "turretRotator", Motor.GoBILDA.RPM_1150);
-    DcMotorEx intake = hardwareMap.get(DcMotorEx.class, "intake");
-    Servo rawTurretHood = hardwareMap.get(Servo.class, "turretHood");
-    Servo lifter1 = hardwareMap.get(Servo.class, "lifter1");
-    lifter1.setDirection(Servo.Direction.REVERSE);
-    Servo lifter2 = hardwareMap.get(Servo.class, "lifter2");
-    AnalogInput lifterEncoder = hardwareMap.get(AnalogInput.class, "lifterEncoder");
-    Axon turretHood = new Axon(rawTurretHood);
-    Axon l1 = new Axon(lifter1);
-    Axon l2 = new Axon(lifter2, lifterEncoder);
-    DualAxon lifter = new DualAxon(l1, l2);
-    Turret turret = new Turret(flywheel, turretRotator, turretHood, 1500);
-    turret.activate();
-    turret.enable();
+    BreakBeam sensor = new BreakBeam(hardwareMap.get(DigitalChannel.class, "breakBeam"));
+    Motor motor = new Motor(hardwareMap, "motor", Motor.GoBILDA.RPM_117);
+    HomableRotator spindex = new HomableRotator(motor, sensor, 0,0, 0, 1, false);
 
     waitForStart();
-    lifter.setPosition(7);
-    intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-    intake.setPower(0);
+    spindex.start();
 
     while (opModeIsActive()) {
-      /*
-      if (gamepad1.dpadUpWasPressed()) {
-        speed += 50;
+      spindex.setP(kP);
+      spindex.setI(kI);
+      spindex.setD(kD);
+      spindex.setTargetAngle(angle);
+      spindex.update();
 
-      } else if (gamepad1.dpadDownWasPressed()) {
-        speed -= 50;
-
-      } else if (gamepad1.dpadRightWasPressed()) {
-        angle += 5;
-
-      } else if (gamepad1.dpadLeftWasPressed()) {
-        angle -= 5;
-      }
-       */
-
-      if (lifter.isAtTarget() && lifter.getTargetPosition() == 100) {
-        lifter.setPosition(7);
-      }
-
-      if (gamepad1.rightBumperWasPressed()) {
-        lifter.setPosition(100);
-      }
-
-      turret.tuneShooting(speed, 60);
-      turret.setHorizontalAngle(0);
-      turret.flywheel.setPIDFCoefficients(
-          DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(kP, kI, kD, kF));
-      turret.update();
-
-      // dashboardTelemetry.addData("Angle", turret.getTargetVerticalAngleDegrees());
-      dashboardTelemetry.addData("Target Speed", turret.testingSpeed);
-      dashboardTelemetry.addData("Speed", turret.currentSpeed);
-      // dashboardTelemetry.addData("At target speed", turret.isReadyToShoot());
-      // dashboardTelemetry.addData("Lifter at target", lifter.isAtTarget());
-      // dashboardTelemetry.addData("Target size", limelight.getTargetSize());
-      // dashboardTelemetry.addData("Limelight locked", limelight.isTargetInFrame());
-      // dashboardTelemetry.addData("At target", turret.isAtTarget());
-      // dashboardTelemetry.addData("Angle", turret.getHorizontalAngleDegrees());
-      // dashboardTelemetry.addData("Target angle", turret.getTargetHorizontalAngleDegrees());
+      dashboardTelemetry.addData("At target", spindex.atTarget());
 
       dashboardTelemetry.update();
     }
